11bc8acb3389659081b140c70b87d1ab3a117f44
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2021-06-19 15:44:36 +0100

    ReActivities first commit

---
+++ Persitance/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persitance
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Action> Activities { get; set; }
+        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        
+    }
+}
